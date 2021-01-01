9e44b2123357e4adf7ee651fe28f379c1faf67d5
Emily Thomas <ethomas@quint.co.uk>
2020-12-31 18:05:32 -0800

    added auto increment for sql file and added department function

--- index.js
+++ index.js
@@ -8,30 +8,28 @@ var tracker = sql.createConnection({
     database:"tracker"
 });
 
-var questions = [
+var question = [
     {
         type: 'list',
-        message: 'What are you adding?',
-        name: 'add' 
-    },
-    {
-        type: 'list',
-        message: 'What do you want to view?',
-        name: 'view'
-    },
+        message: 'What do you want to do?',
+        name: 'initial', 
+        choices: ['add department', 'add employee', 'add role', 'view departments', 'view role', 'view employee', 'update employee role']
+    }];
+
+ var updateEmployeeQuestions = [
     {
         type: 'input',
         message: 'Which employee do you want to update?',
+        name: 'update'
+    },
+    {
+        type: 'input',
+        message: 'What is the new role for this employee?',
         name: 'update'
     }
 ];
 
 var departmentQuestions = [
-    {
-        type: 'input',
-        message: 'What is the department ID?',
-        name: 'departmentId'
-    },
     {
         type: 'input',
         message: 'What is the department name?',
@@ -40,11 +38,6 @@ var departmentQuestions = [
 ];
 
 var roleQuestions = [
-    {
-        type: 'input',
-        message: 'What is the role id?',
-        name: 'roleId'
-    },
     {
         type: 'input',
         message: 'What is your role tile?',
@@ -65,11 +58,6 @@ var roleQuestions = [
 var employeeQuestions = [
     {
         type: 'input',
-        message: 'What is your employee id?',
-        name: 'employeeId'
-    },
-    {
-        type: 'input',
         message: 'What is your first name?',
         name: 'firstName'
     },
@@ -80,25 +68,55 @@ var employeeQuestions = [
     },
     {
         type: 'input',
-        message: 'What is your role id?',
-        name: 'roleId'
-    },
-    {
-        type: 'input',
         message: 'What is your manager id?',
         name: 'managerId'
     }
 ];
 
 tracker.connect(
-    function (
-        err
-    ) {
+    function (err) {
         if (err) {
-            throw err
-        };
-        
-    }
+            throw err  
+        } else {
+            options()
+        }       
+    }  
 );
+function options(){
+    inquirer.prompt(question)
+        .then(function (answer){
+            if (answer.initial === "add department") {
+                inquirer
+                .prompt(departmentQuestions)
+                .then(addDepartment)
+            }
+            if
+        })
+}
+// function for add department
+function addDepartment(answer) {
+    tracker.query("insert into department (departmentName) values (?)", answer.departmentName, function (err) {
+        if (err) 
+            throw err;
+        else {
+            console.log("Success")
+            options()
+        }
+    })
+}
+// add a role
+
+// add an employee
+
+// view department
+function viewDepartments() {
+    // selecting
+}
+// view a role
+
+// view employee
+
+// update employee role
+
 
 
--- index.sql
+++ index.sql
@@ -5,19 +5,19 @@ create database tracker;
 use tracker;
 
 create table department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY auto_increment,
     departmentName VARCHAR(30)
 );
 
 create table employeeRole (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY auto_increment,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 
 create table employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY auto_increment, 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
